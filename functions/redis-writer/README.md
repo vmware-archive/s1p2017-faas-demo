b41d1a6cfa19b25988ca9e52faed8057cc2b91bd
Jurgen Leschner <jldec@ciaosoft.com>
2017-12-01 13:59:29 -0500

    node redis writer

---
+++ functions/redis-writer/README.md
@@ -0,0 +1,13 @@
+** redis-writer (node version)
+
+*** Build the Docker container for the function code
+
+```
+riff build -v 1.0.0
+```
+
+*** Create the Function and Topic(s)
+
+```
+riff apply
+```
---
+++ functions/redis-writer/redis-writer.js
@@ -0,0 +1,53 @@
+module.exports = (input_string) => {
+
+try { var input = JSON.parse(input_string) } 
+catch(err) { return err; }
+
+var { DEFAULT_HASH_KEY, DEFAULT_COMMAND } = process.env
+
+var op = input._operation || DEFAULT_COMMAND 
+delete input._operation; 
+
+var hash = input._hash || DEFAULT_HASH_KEY
+delete input._hash;
+
+const redisOpts = {
+  host:process.env.COUNTERS_REDIS_SERVICE_HOST,
+  port:process.env.COUNTERS_REDIS_SERVICE_PORT,
+  auth_pass:process.env.REDIS_PASSWORD
+}
+
+const redisDB = require('redis').createClient(redisOpts)
+
+redisDB.on('error', (err) => { console.log(`redisDB error: ${err}`); })
+
+if (input._list) {
+  const key = input._list
+  delete input._list
+  redisDB.rpush(key, JSON.stringify(input), redisErr)
+}
+else 
+{
+  for (key in input) {
+    if (/^_/.test(key)) continue;
+
+    if (hash) {
+      if (op == 'increment') {
+        redisDB.hincrby(hash, key, input[key], redisErr)
+      } else {
+        redisDB.hset(hash, key, input[key], redisErr)
+      }
+    } else {
+      if (op == 'increment') {
+        redisDB.incrby(key, input[key], redisErr)
+      } else {
+        redisDB.set(key, input[key], redisErr)
+      }
+    }
+  }
+}
+
+function redisErr(err) { if (err) { console.log(err) }}
+
+return input_string;
+} 
