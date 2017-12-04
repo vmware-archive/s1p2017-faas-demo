490bd4e4f7933a569ca03326355be1a8a0dcc762
markfisher <mfisher@pivotal.io>
2017-12-04 13:29:00 -0800

    simplified word-counter

--- functions/word-counter/src/main/java/functions/WordCounter.java
+++ functions/word-counter/src/main/java/functions/WordCounter.java
@@ -7,14 +7,11 @@ import java.util.function.Function;
 
 import reactor.core.publisher.Flux;
 
-public class WordCounter implements Function<Flux<String>, Flux<Map<String,Integer>>> {
-
+public class WordCounter implements Function<Flux<String>, Flux<Map<String, Integer>>> {
 	public Flux<Map<String, Integer>> apply(Flux<String> words) {
-		return words.window(Duration.ofSeconds(2))
-				.flatMap(f -> f.flatMap(word -> Flux.fromArray(word.split("\\W")))
-				.reduce(new HashMap<String, Integer>(), (map, word) -> {
-						map.merge(word, 1, Integer::sum);
-						return map;
-				}));
+		return words.flatMap(word -> Flux.fromArray(word.split("\\W")))
+				.window(Duration.ofSeconds(10))
+				.flatMap(w -> w.collect(HashMap::new, (map, word) ->
+								map.merge(word, 1, Integer::sum)));
 	}
 }
